365e013ff437551f29b85db230b8022d4631b4fb
harshjain883 <115611849+harshjain883@users.noreply.github.com>
2026-01-11 21:26:39 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -18,7 +18,7 @@ app = Flask(__name__,
 CORS(app)
 
 # JioSaavn API
-API_BASE = "https://jiosaavnapi-nu.vercel.app"
+API_BASE = "https://saavn.sumit.co/"
 
 def make_request(endpoint, params=None):
     """Make API request with error handling"""
